9c75cee223a84276b6828b217f708534940db0bb
Dan Brunwasser <dan.brunwasser@infor.com>
2017-06-06 15:12:55 -0400

    fixed merge issue

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,17 +22,12 @@ import Label from '../components/Label';
 import { getAuthToken } from '../restGet';
 
 export default class Login extends Component {
-<<<<<<< HEAD
 
 	static navigationOptions = {
 		title: 'Welcome',
 	};
 
-
-	constructor(props){
-=======
 	constructor(props) {
->>>>>>> b9cc9506c8be006bbd2c6aa7947b67e273af6a66
 		super(props);
 
 		this.state = {
@@ -80,36 +75,6 @@ export default class Login extends Component {
 						disabled={this.state.loading}
 					/>
 
-<<<<<<< HEAD
-			<Container>
-				<Button
-					onPress = {this._handleLogin.bind(this)}
-					title= "Login"
-					backgroundColor = "#8DC9E6"	
-				/>
-
-				<ActivityIndicator animating={this.state.loading} size="large"/>
-			</Container>		
-
-        </ScrollView>
-    );
-  }
-
-  _handleLogin(event) {
-  	
-
- 		this.setState({loading : true});
-  		var response = getAuthToken(dataKey, this.state.username, this.state.password, this.state.eid, (response) => {
-  			this.setState({loading: false});
-  			if (response.status!=200){
-  				Alert.alert('error');
-  			}
-  			console.log(response);
-  		});
-
-	}
-
-=======
 					<ActivityIndicator animating={this.state.loading} size="large" />
 				</Container>
 
@@ -118,74 +83,18 @@ export default class Login extends Component {
 	}
 
 	_handleLogin(event) {
+
+
 		this.setState({ loading: true });
-
-		if (this.state.eid != '') {
-			var toEncode = this.state.username + ":" + this.state.password + ":" + this.state.eid;
-		} else {
-
-			var toEncode = this.state.username + ":" + this.state.password;
-		}
-		var base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-		var padding = "";
-		var encodedString = "";
-
-		//checking if credential string has length of multiple of 3
-		if ((toEncode.length % 3) > 0) {
-			var i = (toEncode.length % 3);
-			for (; i < 3; i++) {
-				padding += "=";
-				toEncode += "\0";
+		var response = getAuthToken(dataKey, this.state.username, this.state.password, this.state.eid, (response) => {
+			this.setState({ loading: false });
+			if (response.status != 200) {
+				Alert.alert('error');
 			}
-		}
-
-		//iterating through all triplets in input string
-		for (var i = 0; i < toEncode.length; i += 3) {
-
-			//every triplet gets mapped to a 24 bit number
-			var n = (toEncode.charCodeAt(i) << 16) + (toEncode.charCodeAt(i + 1) << 8) + (toEncode.charCodeAt(i + 2));
-
-			//this new number gets seperated into four 6 bit numbers
-			n = [(n >>> 18) & 63, (n >>> 12) & 63, (n >>> 6) & 63, n & 63];
-
-			//use new numbers as indices from base64 char list
-			encodedString += base64chars[n[0]] + base64chars[n[1]] + base64chars[n[2]] + base64chars[n[3]];
-
-		}
-		encodedString = encodedString.substring(0, encodedString.length - padding.length) + padding;
-
-		//to create final token add "Basic "
-
-		var finalToken = "Basic " + encodedString;
-		var res;
-
-		//getting repsponse from api
-
-		fetch('https://demo.gtnexus.com/rest/310?dataKey=' + dataKey,
-			{
-				method: "GET",
-				headers: {
-					"Authorization": finalToken,
-					"Content-Type": "application/json"
-				}
-			}).then(
-			function (response) {
-				if (response.status !== 200) {
-					this.setState({ loading: false });
-					Alert.alert('Error', 'Authentication Failed' + "\n" + response.status + "\n" + finalToken);
-					return;
-				}
-
-				Alert.alert('Success', response.statusText);
-				this.setState({ loading: false });
-			}.bind(this)
-
-			)
+			console.log(response);
+		});
 
 	}
-}
->>>>>>> b9cc9506c8be006bbd2c6aa7947b67e273af6a66
-
 }
 
 
