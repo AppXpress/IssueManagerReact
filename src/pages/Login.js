daa1114d6479420ca8e9e50fa2e3f43878cf20c5
Dan Brunwasser <dan.brunwasser@infor.com>
2017-06-06 14:41:38 -0400

    added soho style button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,9 +10,10 @@ import {
 	TextInput,
 	ScrollView,
 	StatusBar,
-	Button,
 	ActivityIndicator
 } from 'react-native';
+
+import Button from '../SoHo/Button';
 
 import Container from '../components/Container';
 
@@ -20,7 +21,7 @@ import Label from '../components/Label';
 
 
 export default class Login extends Component {
-	constructor(props){
+	constructor(props) {
 		super(props);
 
 		this.state = {
@@ -31,128 +32,129 @@ export default class Login extends Component {
 		};
 	}
 
-  render() {
-    return (
+	render() {
+		return (
 
-        <ScrollView style={styles.scroll}>
-        	<Container>
-        	</Container>
-        	<Container>
-        	
-    			<TextInput
-        			style={styles.inp}
-        			placeholder= 'Username'
-        			onChangeText={(text) => this.setState({username:text})}
-        			autoCapitalize= 'none'
-        			/>
-    		
-    			<TextInput
-        			secureTextEntry={true}
-        			style={styles.inp}
-        			placeholder='Password'
-        			onChangeText={(text) => this.setState({password:text})}
-    			/>
-    			<TextInput
-    				secureTextEntry={true}
-    				style = {styles.inp}
-    				placeholder = 'EID (Leave Blank if Unnecesarry)'
-    				onChangeText ={(text) => this.setState({eid:text})}
-    			/>	
+			<ScrollView style={styles.scroll}>
+				<Container>
+				</Container>
+				<Container>
+
+					<TextInput
+						style={styles.inp}
+						placeholder='Username'
+						onChangeText={(text) => this.setState({ username: text })}
+						autoCapitalize='none'
+					/>
+
+					<TextInput
+						secureTextEntry={true}
+						style={styles.inp}
+						placeholder='Password'
+						onChangeText={(text) => this.setState({ password: text })}
+					/>
+					<TextInput
+						secureTextEntry={true}
+						style={styles.inp}
+						placeholder='EID (Leave Blank if Unnecesarry)'
+						onChangeText={(text) => this.setState({ eid: text })}
+					/>
 				</Container>
 
-			<Container>
-				<Button
-					onPress = {this._handleLogin.bind(this)}
-					title= "Login"
-					backgroundColor = "#8DC9E6"	
-				/>
+				<Container>
+					<Button
+						onPress={this._handleLogin.bind(this)}
+						title="Login"
+						primary
+						disabled={this.state.loading}
+					/>
 
-				<ActivityIndicator animating={this.state.loading} size="large"/>
-			</Container>		
+					<ActivityIndicator animating={this.state.loading} size="large" />
+				</Container>
 
-        </ScrollView>
-    );
-  }
-
-  _handleLogin(event) {
-  	this.setState({loading:true});
-
-  	if (this.state.eid!=''){
-  		var toEncode = this.state.username + ":" +this.state.password + ":" +this.state.eid;
-  	}else{
-
-  	var toEncode  = this.state.username + ":" +this.state.password;
-  }
-  	var base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-  	var padding = "";
-  	var encodedString = "";
-  	
-  	//checking if credential string has length of multiple of 3
-  	if ((toEncode.length % 3) >0){
-  		var i = (toEncode.length % 3);
-  		for(; i < 3; i++){
-  			padding+= "=";
-  			toEncode+= "\0";
-  		}
+			</ScrollView>
+		);
 	}
 
-	//iterating through all triplets in input string
-	for(var i=0; i < toEncode.length; i+=3){
+	_handleLogin(event) {
+		this.setState({ loading: true });
 
-		//every triplet gets mapped to a 24 bit number
-		var n = (toEncode.charCodeAt(i)<<16) + (toEncode.charCodeAt(i+1)<<8) + (toEncode.charCodeAt(i+2));
+		if (this.state.eid != '') {
+			var toEncode = this.state.username + ":" + this.state.password + ":" + this.state.eid;
+		} else {
 
-		//this new number gets seperated into four 6 bit numbers
-		n = [(n >>> 18) & 63, (n >>> 12) & 63, (n >>> 6) & 63, n & 63];
+			var toEncode = this.state.username + ":" + this.state.password;
+		}
+		var base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+		var padding = "";
+		var encodedString = "";
 
-		//use new numbers as indices from base64 char list
-		encodedString += base64chars[n[0]] + base64chars[n[1]] + base64chars[n[2]] + base64chars[n[3]];
-
-	}
-	encodedString = encodedString.substring(0, encodedString.length - padding.length) + padding;
-
-	//to create final token add "Basic "
-
-	var finalToken = "Basic " + encodedString;
-	var res;
-
-	//getting repsponse from api
-
-	fetch('https://demo.gtnexus.com/rest/310?dataKey=' + dataKey,
-		{
-			method: "GET",
-			headers: {
-				"Authorization" : finalToken,
-				"Content-Type" : "application/json"
+		//checking if credential string has length of multiple of 3
+		if ((toEncode.length % 3) > 0) {
+			var i = (toEncode.length % 3);
+			for (; i < 3; i++) {
+				padding += "=";
+				toEncode += "\0";
 			}
-		}).then(
-			function(response) {
-				if (response.status!== 200){
-						this.setState({loading:false});
-					Alert.alert('Error', 'Authentication Failed' + "\n" +response.status +"\n"+ finalToken);
+		}
+
+		//iterating through all triplets in input string
+		for (var i = 0; i < toEncode.length; i += 3) {
+
+			//every triplet gets mapped to a 24 bit number
+			var n = (toEncode.charCodeAt(i) << 16) + (toEncode.charCodeAt(i + 1) << 8) + (toEncode.charCodeAt(i + 2));
+
+			//this new number gets seperated into four 6 bit numbers
+			n = [(n >>> 18) & 63, (n >>> 12) & 63, (n >>> 6) & 63, n & 63];
+
+			//use new numbers as indices from base64 char list
+			encodedString += base64chars[n[0]] + base64chars[n[1]] + base64chars[n[2]] + base64chars[n[3]];
+
+		}
+		encodedString = encodedString.substring(0, encodedString.length - padding.length) + padding;
+
+		//to create final token add "Basic "
+
+		var finalToken = "Basic " + encodedString;
+		var res;
+
+		//getting repsponse from api
+
+		fetch('https://demo.gtnexus.com/rest/310?dataKey=' + dataKey,
+			{
+				method: "GET",
+				headers: {
+					"Authorization": finalToken,
+					"Content-Type": "application/json"
+				}
+			}).then(
+			function (response) {
+				if (response.status !== 200) {
+					this.setState({ loading: false });
+					Alert.alert('Error', 'Authentication Failed' + "\n" + response.status + "\n" + finalToken);
 					return;
 				}
-				
-				Alert.alert('Success', response.statusText);
-				this.setState({loading:false});
-			}.bind(this)
-			
-		)	
 
-  }
+				Alert.alert('Success', response.statusText);
+				this.setState({ loading: false });
+			}.bind(this)
+
+			)
+
+	}
 }
 
 
 
 const styles = StyleSheet.create({
 	scroll: {
-    	backgroundColor: '#F0F0F0',
-    	padding: 0,
-    	flexDirection: 'column'
+		backgroundColor: '#F0F0F0',
+		padding: 0,
+		flexDirection: 'column'
 	},
 	label: {
 		color: 'black',
-    	fontSize: 28
+		fontSize: 28
 	},
 	inp: {
 		backgroundColor: '#FFFFFF',
