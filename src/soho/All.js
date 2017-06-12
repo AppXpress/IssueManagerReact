8113f94f4bda5cb2fd7098b28958a39a89bf8b5d
Dan Brunwasser <dan.brunwasser@infor.com>
2017-06-12 12:06:41 -0400

    added soho picker and org picker

--- src/soho/All.js
+++ src/soho/All.js
@@ -4,22 +4,24 @@ import Field from './Field';
 import ListCard from './ListCard';
 import Navigataion from './Navigation';
 import Page from './Page';
+import Picker from './Picker';
 import Switch from './Switch';
 import TextInput from './TextInput';
 
 import { getColor } from './Tools.js';
 
-const SoHo = {
+const soho = {
 	get Button() { return Button; },
 	get Card() { return Card; },
 	get Field() { return Field; },
 	get ListCard() { return ListCard; },
 	get Navigataion() { return Navigataion; },
 	get Page() { return Page; },
+	get Picker() { return Picker; },
 	get Switch() { return Switch },
 	get TextInput() { return TextInput; },
 
 	get getColor() { return getColor; }
 }
 
-module.exports = SoHo;
+module.exports = soho;
