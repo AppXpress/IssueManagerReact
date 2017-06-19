b82b88ca485c637b3591ca4a23651683cd7d34b1
ptz3233 <ptz3233@gmail.com>
2017-06-19 13:08:25 -0400

    Added advanced filtering functionality

--- src/pages/IssueList.js
+++ src/pages/IssueList.js
@@ -25,7 +25,9 @@ import {
 	ListItem,
 	Navigataion,
 	Page,
-	TextInput
+	TextInput,
+	Modal,
+	Picker,
 } from '../soho/All';
 
 export default class IssueList extends Component {
@@ -36,7 +38,12 @@ export default class IssueList extends Component {
 		this.props = props;
 
 		this.state = {
-			searchtext: ''
+			searchtext: '',
+			modalVisible: false,
+			filterBy: 'subject',
+			severityFilter: null,
+			typeFilter: null,
+			daysFilter: null,
 		};
 	}
 
@@ -76,6 +83,7 @@ export default class IssueList extends Component {
 		var data = await AppX.query('$IssueT3', 'ORDER BY modifyTimestamp DESC');
 		if (data) {
 			this.setState({
+				fullIssues: data.result,
 				issues: data.result,
 				loading: false
 			});
@@ -84,20 +92,62 @@ export default class IssueList extends Component {
 		}
 	}
 
+	async requery(){
+		this.setState({
+			issues: null,
+			loading: true
+		});
+
+		if(this.state.severityFilter){
+
+		}
+
+		var oqlStmnt = this.state.filterBy + ' contains ' +'\'' + this.state.searchtext+ '\'';
+
+		if(this.state.severityFilter){
+			oqlStmnt += " AND severity = \'" + this.state.severityFilter + "\'";
+		}
+
+		if(this.state.typeFilter){
+			oqlStmnt += " AND type = \'" + this.state.typeFilter + "\'";
+		}
+		if(this.state.daysFilter){
+			oqlStmnt += " AND createdOn In @(Last "+this.state.daysFilter+" Days)"
+		}
+        
+        oqlStmnt+= " ORDER BY modifyTimestamp DESC";
+
+		var data = await AppX.query('$IssueT3', oqlStmnt);
+		if (data) {
+			this.setState({
+				fullIssues: data.result,
+				issues: data.result,
+				loading: false
+			});
+		} else {
+			alert('We weren\'t able to load any issues. Please try again later!');
+		}
+
+
+
+	}
+
 	setSearchText(event) {
 		let searchText = event;
 		this.setState({ searchtext: searchText });
 
-		let filteredData = this.filterIssues(searchText, this.state.issues);
+		let filteredData = this.filterIssues(searchText, this.state.fullIssues);
 		this.setState({ issues: filteredData });
 	}
 
 	filterIssues(searchText, issueData) {
 		let text = searchText.toLowerCase();
 
+		var filterBy = this.state.filterBy;
+
 		return issueData.filter((n) => {
-			if (n.subject) {
-				let iss = n.subject.toLowerCase();
+			if (n[filterBy]) {
+				let iss = n[filterBy].toLowerCase();
 				return iss.search(text) !== -1;
 			}
 		});
@@ -118,13 +168,81 @@ export default class IssueList extends Component {
 	render() {
 		return (
 			<Page>
-				<ListItem>
+			<Modal
+				visible={this.state.modalVisible}
+				onClose={() => this.setState({ modalVisible: false })}
+				onRequestClose={() => this.setState({ modalVisible: false })}
+				>
+
+				<Picker
+						label='Filter By'
+						title='Select a field to filter by'
+						selectedValue={this.state.filterBy}
+						onValueChange={(item, index) => this.setState({ filterBy: item })}
+					>
+						<Picker.Item label="Subject" value="subject" />
+						<Picker.Item label="Creator" value="createdBy" />
+						<Picker.Item label="Description" value="description" />
+					</Picker>
+				<TextInput
+						label='Search'
+						onChangeText={(text) => this.setState({searchtext: text})}
+						autoCapitalize='none'
+					/> 
+
+				<Picker
+						label='Severity'
+						title='Select a severity to filter by'
+						selectedValue={this.state.severityFilter}
+						onValueChange={(item, index) => this.setState({ severityFilter: item })}
+					>
+						<Picker.Item label="High" value="3" />
+						<Picker.Item label="Medium" value="2" />
+						<Picker.Item label="Low" value="1" />
+						<Picker.Item label="None" value={null} />
+					</Picker>
+				<Picker
+						label='Issue Type'
+						title='Select an issue type to filter by'
+						selectedValue={this.state.typeFilter}
+						onValueChange={(item, index) => this.setState({ typeFilter: item })}
+					>
+						<Picker.Item label="Shipping" value='1' />
+						<Picker.Item label="Factory Supply" value='2' />
+						<Picker.Item label="Quality Control" value='3' />
+						<Picker.Item label="None" value={null} />
+					</Picker>
+				<TextInput
+						label="Created in last X days"
+						onChangeText={(text) => this.setState({daysFilter: text})}
+						keyboardType={'numeric'}
+						returnKeyType={'done'}
+					
+					/>
+				<Button
+					title='search'
+					onPress={()=> {this.setState({modalVisible:false}); this.requery()}}
+					/>	
+
+			</Modal>			
+
+				
+			{/*	<ListItem>
 					<TextInput
 						label='Search'
 						onChangeText={this.setSearchText.bind(this)}
 						autoCapitalize='none'
-					/>
+					/> 
+				</ListItem> */}
+				<ListItem>
+					<Button
+						title='filter'
+						onPress={()=> this.setState({modalVisible: true, searchtext: '', daysFilter: null})}
+						>
+					</Button>	
+
 				</ListItem>
+
 
 				{this.state.loading &&
 					<ActivityIndicator animating={true} size="large" />
