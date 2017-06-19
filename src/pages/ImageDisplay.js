caf3604282f923b24b6565e19f8caf005a75ea85
Jack Yudt <jyudt@usnymjyudt01.infor.com>
2017-06-19 15:39:06 -0400

    added image display page

---
+++ src/pages/ImageDisplay.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { NavigationActions } from 'react-navigation';
+
+import {
+	Alert,
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	ScrollView,
+	StatusBar,
+	ActivityIndicator,
+	AsyncStorage,
+	ListView,
+  Image
+} from 'react-native';
+
+import {
+	AppX
+} from '../gtn/All';
+
+import {
+	Button,
+	Card,
+	Field,
+	List,
+	Navigataion,
+	Page,
+	TextInput
+} from '../soho/All';
+
+
+export default class ImageDisplay extends Component {
+
+  constructor(props) {
+		super(props);
+    this.state = {
+		    root: this.props.image,
+      }
+    console.log(this.state.root);
+
+	}
+
+  render() {
+    return (
+      <View>
+        <Image
+          style={{width: 50, height: 50}}
+          source={this.state.root}
+        />
+      </View>
+    );
+  }
+}
