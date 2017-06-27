e4a3b663e4230c0f352cfcbb46e16ebeaf978e3b
ptz3233 <ptz3233@gmail.com>
2017-06-27 10:29:30 -0400

    added camera fixes

--- src/pages/CameraScreen.js
+++ src/pages/CameraScreen.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 import {
     View,
+    StyleSheet,
+    Dimensions,
 } from 'react-native';
+
+import {
+	Icon,
+} from '../soho/All'
 
 import Camera from 'react-native-camera';
 
@@ -17,12 +23,42 @@ export default class CameraScreen extends Component {
 
     render(){
     	return(
-    		<View>
-    			<Camera />
+    		<View style={styles.container} >
+    			<Camera 
+    			        ref={(cam) => {
+            			this.camera = cam;
+          				}}
+          			aspect={Camera.constants.Aspect.fill}
+          			style={styles.preview}>
+          			<Icon icon='upload' />
+
+          			</Camera>	
     		</View>	
 
 
     		);
 
     }
-}    
+}
+
+const styles = StyleSheet.create({
+
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  			},
+
+  	container: {
+    flex: 1,
+    flexDirection: 'row',
+  			},
+  	capture: {
+  		flex: 0,
+  		borderRadius: 5,
+  		padding: 10,
+  		margin: 40,
+  	},			
+  });    
