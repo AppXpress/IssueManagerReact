9ff6e514094424aafcdedd6f180b631c367d321e
Dan Brunwasser <dan.brunwasser@infor.com>
2017-06-27 12:33:40 -0400

    moved index to one location

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import { Navigation } from 'react-native-navigation';
+
+import Login from './pages/Login';
+import IssueList from './pages/IssueList';
+import FilterIssues from './pages/FilterIssues';
+import IssueDetails from './pages/IssueDetails';
+import CreateIssue from './pages/CreateIssue';
+import CreateMessage from './pages/CreateMessage';
+import CameraScreen from './pages/CameraScreen';
+import CameraDisplay from './pages/CameraDisplay'
+
+/**
+*	Entry-point for all devices. Registers all necesarry pages
+*	to the navigation library.
+*/
+Navigation.registerComponent('Login', () => Login);
+Navigation.registerComponent('IssueList', () => IssueList);
+Navigation.registerComponent('FilterIssues', () => FilterIssues)
+Navigation.registerComponent('IssueDetails', () => IssueDetails);
+Navigation.registerComponent('CreateIssue', () => CreateIssue);
+Navigation.registerComponent('CreateMessage', () => CreateMessage);
+Navigation.registerComponent('CameraScreen', () => CameraScreen);
+Navigation.registerComponent('CameraDisplay', () => CameraDisplay);
+
+Navigation.startSingleScreenApp({
+    screen: { screen: 'Login' }
+});
--- src/pages/ImageDisplay.js
+++ src/pages/ImageDisplay.js
@@ -2,15 +2,8 @@ import React, { Component } from 'react';
 import { NavigationActions } from 'react-navigation';
 
 import {
-	Alert,
-	AppRegistry,
 	StyleSheet,
-	Text,
 	View,
-	ScrollView,
-	StatusBar,
-	AsyncStorage,
-	ListView,
 	Image
 } from 'react-native';
 
@@ -33,21 +26,26 @@ export default class ImageDisplay extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {
-			root: this.props.image,
-		}
-		console.log(this.state.root);
-
 	}
 
 	render() {
 		return (
-			<View>
+			<View style={styles.view}>
 				<Image
-					style={{ width: 50, height: 50 }}
-					source={this.state.root}
+					style={styles.image}
+					source={{ url: this.props.image }}
 				/>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	view: {
+		flex: 1,
+		alignContents: 'stretch'
+	},
+	image: {
+		flex: 1
+	}
+});
